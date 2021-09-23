a200e17b2c6b4d68dd0afe7cf8985ba99a626111
shumei <1284591101@qq.com>
2021-09-23 15:08:56 +0800

    main提交

--- AAA.py
+++ AAA.py
@@ -1,10 +1,8 @@
 # 新增一个文件提交
 
-<<<<<<< HEAD
 #在test2对AAASA进行修改提交
 print ("测试在test2对AAAA进行修改提交")
 print("提交到本地")
-=======
 # 在test2对AAASA进行修改提交
 print("测试在test2对AAAA进行修改提交")
 
@@ -18,4 +16,3 @@ print('*' * 9)
 print('*' * 8)
 print('*' * 7)
 print('*' * 6)
->>>>>>> 5e02c97e7b000fdfd197bfd21944f8c76d27c876
